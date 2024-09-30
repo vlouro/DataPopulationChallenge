e7c361ae6828a9ad9e3060f50c903c27a063323d
moongyinterprojects <valterlouro@gmail.com>
2024-09-30 21:15:30 +0100

    Commit 1 - Created Project
    Created Project
    Changed minimum version to iOS 17

---
+++ DataPopulation/DataPopulation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataPopulation
+//
+//  Created by Valter Louro on 30/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DataPopulation/DataPopulationTests/DataPopulationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DataPopulationTests.swift
+//  DataPopulationTests
+//
+//  Created by Valter Louro on 30/09/2024.
+//
+
+import Testing
+@testable import DataPopulation
+
+struct DataPopulationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
